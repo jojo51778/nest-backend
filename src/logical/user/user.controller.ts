0b1d3c6e173b035b284843ce9c80940c9e5cf0b3
陈章闻 <623025156@qq.com>
2020-04-14 15:39:31 +0800

    add swagger ui

--- src/logical/user/user.controller.ts
+++ src/logical/user/user.controller.ts
@@ -3,20 +3,23 @@ import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from '../auth/auth.service'
 import { UserService } from './user.service'
 import { ValidationPipe } from '../../pipe/validation.pipe';
-import { RegisterInfoDTO } from './user.dto'; // 引入 DTO
+import { FindDTO, LoginDTO, RegisterInfoDTO } from './user.dto'; // 引入 DTO
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiBearerAuth() // Swagger 的 JWT 验证
+@ApiTags('user') // 添加 接口标签 装饰器
 @Controller('user')
 export class UserController {
   constructor(private readonly authService: AuthService, private readonly usersService: UserService) {}
 
   @UseGuards(AuthGuard('jwt'))
   @Post('find-one')
-  findOne(@Body() body:any) {
+  findOne(@Body() body: FindDTO) {
     return this.usersService.findOne(body.username)
   }
 
   @Post('login')
-  async login(@Body() loginParams: any) {
+  async login(@Body() loginParams: LoginDTO) {
     console.log('JWT验证 - Step 1: 用户请求登录')
     const authResult = await this.authService.validateUser(loginParams.username, loginParams.password);
     switch(authResult.code) {
