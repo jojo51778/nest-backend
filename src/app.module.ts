23e4dec7522a776ecb4d79e15dc10ca88a117d6e
陈章闻 <623025156@qq.com>
2020-04-14 14:42:15 +0800

    add rbac 0

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './logical/user/user.module';
 import { AuthModule } from './logical/auth/auth.module';
 import { UserController } from './logical/user/user.controller';
+import { CommodityService } from './logical/commodity/commodity.service';
+import { CommodityController } from './logical/commodity/commodity.controller';
 
 @Module({
   imports: [UserModule, AuthModule],
-  providers: [],
-  controllers: [UserController]
+  providers: [CommodityService],
+  controllers: [UserController, CommodityController]
 })
 export class AppModule {}
